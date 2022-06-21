49e23771dd3cbaf25d63f0e5de49a5c0f360b2c5
parryk303 <kyle.parry.303@gmail.com>
2022-06-21 11:41:20 -0600

    Add ssaqs, tube, approved_awaiting, ratings

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
     // password: '',
-    database: 'todo_db',
+    database: 'cyberfusion_db',
     host: 'localhost',
     port: 5432
 })
--- index.js
+++ index.js
@@ -1,72 +1,163 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const pool = require('./db')
+const pool = require('./db');
 
 app.use(cors({
     origin: '*',
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
 app.use(express.json()) // req.body
 
+//CYBERFUSION HTTP
+
 // CREATE
-app.post('/todos', async (req, res) => {
+app.post('/ssaqs', async (req, res) => {
     try {
-        const { description } = req.body;
-        const newTodo = await pool.query(
-            'INSERT INTO todo (description) VALUES ($1) RETURNING *',
-            [description])
-        res.json(newTodo.rows[0]);
+        const { ssaqs } = req.body;
+        const newData = await pool.query(
+            'INSERT INTO ssaqs (ssaqs) VALUES ($1) RETURNING *',
+            [ssaqs])
+        res.json(newData.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// CREATE
+app.post('/tube', async (req, res) => {
+    try {
+        const { tube } = req.body;
+        const newData = await pool.query(
+            'INSERT INTO tube (tube) VALUES ($1) RETURNING *',
+            [tube])
+        res.json(newData.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// CREATE
+app.post('/a_a', async (req, res) => {
+    try {
+        const { approved_awaiting } = req.body;
+        const newData = await pool.query(
+            'INSERT INTO approved_awaiting (approved_awaiting) VALUES ($1) RETURNING *',
+            [approved_awaiting])
+        res.json(newData.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// CREATE
+app.post('/ratings', async (req, res) => {
+    try {
+        const { ratings } = req.body;
+        const newData = await pool.query(
+            'INSERT INTO ratings (ratings) VALUES ($1) RETURNING *',
+            [ratings])
+        res.json(newData.rows[0]);
     } catch (err) {
         console.error(err.message)
     }
 })
 
 // READ all
-app.get('/todos', async (req, res) => {
+app.get('/cyberfusion', async (req, res) => {
     try {
-        const allTodos = await pool.query('SELECT * FROM todo')
-        res.json(allTodos.rows);
+        const allcyberfusion = await pool.query('SELECT * FROM cyberfusion')
+        res.json(allcyberfusion.rows);
     } catch (err) {
         console.error(err.message)
     }
 })
 
-// READ id
-app.get('/todos/:id', async (req, res) => {
+// READ ssaqs
+app.get('/ssaqs', async (req, res) => {
     try {
-        const { id } = req.params;
-        const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id])
-        res.json(todo.rows[0]);
+        const ssaqsD = await pool.query('SELECT * FROM ssaqs')
+        res.json(ssaqsD.rows[0].ssaqs);
     } catch (err) {
         console.error(err.message)
     }
 })
 
-// UPDATE
-app.put('/todos/:id', async (req, res) => {
+// READ tube
+app.get('/tube', async (req, res) => {
     try {
-        const { id } = req.params;
-        const { description } = req.body;
-        await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2', [description, id])
-        res.json('todo was sucessfully updated');
+        const tubeD = await pool.query('SELECT * FROM tube')
+        res.json(tubeD.rows[0].tube);
     } catch (err) {
         console.error(err.message)
     }
 })
 
-// DELETE
-app.delete('/todos/:id', async (req, res) => {
+// READ approved_awaiting
+app.get('/a_a', async (req, res) => {
     try {
-        const { id } = req.params;
-        await pool.query('DELETE FROM todo WHERE todo_id = $1', [id])
-        res.json('todo was sucessfully deleted');
+        const approved_awaitingD = await pool.query('SELECT * FROM approved_awaiting')
+        res.json(approved_awaitingD.rows[0].approved_awaiting);
     } catch (err) {
         console.error(err.message)
     }
 })
 
-app.listen(3000, () => {
-    console.log('server is listenting on port 3000')
+// READ ratings
+app.get('/ratings', async (req, res) => {
+    try {
+        const ratingsD = await pool.query('SELECT * FROM ratings')
+        res.json(ratingsD.rows[0].ratings);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// UPDATE ssaqs
+app.put('/ssaqs', async (req, res) => {
+    try {
+        const { ssaqs } = req.body;
+        await pool.query('UPDATE ssaqs SET ssaqs = $1', [ssaqs])
+        res.json('ssaqs was sucessfully updated');
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// UPDATE tube
+app.put('/tube', async (req, res) => {
+    try {
+        const { tube } = req.body;
+        await pool.query('UPDATE tube SET tube = $1', [tube])
+        res.json('tube was sucessfully updated');
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// UPDATE ratings
+app.put('/ratings', async (req, res) => {
+    try {
+        const { ratings } = req.body;
+        await pool.query('UPDATE ratings SET ratings = $1', [ratings])
+        res.json('ratings was sucessfully updated');
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// UPDATE approved_awaiting
+app.put('/a_a', async (req, res) => {
+    try {
+        const { approved_awaiting } = req.body;
+        await pool.query('UPDATE approved_awaiting SET approved_awaiting = $1', [approved_awaiting])
+        res.json('approved_awaiting was sucessfully updated');
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.listen(8888, () => {
+    console.log('server is listenting on port 8888')
 })
