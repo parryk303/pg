708e4df257c1d4f0212b239ad6517a4e7588da43
parryk303 <kyle.parry.303@gmail.com>
2022-06-24 10:41:49 -0600

    Update certs

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const https = require('https');
 const fs = require('fs');
 const port = 8888;
 
-var key = fs.readFileSync(__dirname + '/certs/sr_cyberfusion.securonix.net-key.pem');
-var cert = fs.readFileSync(__dirname + '/certs/sr_cyberfusion.securonix.net.pem');
+var key = fs.readFileSync(__dirname + '/certs/securonix.net-key.pem');
+var cert = fs.readFileSync(__dirname + '/certs/securonix.net.pem');
 var options = {
   key: key,
   cert: cert
